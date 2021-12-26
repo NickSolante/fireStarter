9b67aa16d3f294aaddf45fc5171e83e20663bc14
NickSolante <41457938+NickSolante@users.noreply.github.com>
2021-12-26 14:57:58 +1100

    wip: remove url

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import axios, { AxiosResponse } from "axios";
 
 const path =
   process.env.NODE_ENV === "production"
-    ? "https://firebrand-v8fa3.ondigitalocean.app"
+    ? process.env.API_URL
     : "http://localhost:3000";
 
 const qrCodes = async (body: string): Promise<AxiosResponse<string, any>> => {
