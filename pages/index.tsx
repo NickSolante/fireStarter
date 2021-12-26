d6f125d6902ca5621b13b09a6fc56d863b4c904a
NickSolante <41457938+NickSolante@users.noreply.github.com>
2021-12-26 16:22:03 +1100

    wip: remove url

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const path = "https://firebrand-v8fa3.ondigitalocean.app";
 
 const qrCodes = async (body: string): Promise<AxiosResponse<string, any>> => {
   const res = await axios.post(`${path}/scan`, { url: body });
-  console.log(process.env.API_URL);
+  console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
   console.log(res);
   return res;
 };
